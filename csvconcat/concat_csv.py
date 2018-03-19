cc0ce7e6161a17ef883ab2b7685061e31af1f02e
Subhash Ramesh Deshmukh <subhash.deshmukh@rudderanalytics.com>
2018-03-19 16:15:47 +0530

    Initial push

--- csvconcat/concat_csv.py
+++ csvconcat/concat_csv.py
@@ -1,4 +1,6 @@
-#contacting two csv files using pandas
+#concat two csv files provided they have similar data
+#add another column with name of first file
+#write this data to the third file
 
 import pandas as pd
 
@@ -10,10 +12,11 @@ for file in csv_files:
     df = pd.read_csv(file,sep='\t')
     final_df = pd.concat([final_df,df])
 
-#print final_df.head(10)
 final_df['source'] = 'first.csv'
 
 print(final_df.head())
+final_df.to_csv('third.csv',sep='\t')
+
 
 
 
