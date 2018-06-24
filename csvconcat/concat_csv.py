0030078c06c19fc476c78af61972e4abb98725c7
Subhash Deshmukh <subhash.r.deshmukh92@gmail.com>
2018-06-24 09:39:46 +0530

    comments are added to the code'

--- csvconcat/concat_csv.py
+++ csvconcat/concat_csv.py
@@ -4,18 +4,25 @@
 
 import pandas as pd
 
+
+# the list of csv sample files
 csv_files = ['first.csv','second.csv']
 
+# create an empty dataframe
 final_df = pd.DataFrame()
 
+# read the file in the pandas dataframe
 for file in csv_files:
     df = pd.read_csv(file,sep='\t')
     final_df = pd.concat([final_df,df])
 
+
 final_df['source'] = 'first.csv'
 
+# printing the top rows of the dataframe
 print(final_df.head())
 final_df.to_csv('third.csv',sep='\t')
+
 
 
 
